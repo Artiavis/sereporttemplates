e8667d45f8b64590f684228346081ae0a51b6e18
Jeff Rabinowitz <rabinowitzjeffc@gmail.com>
2013-04-17 15:35:23 -0400

    Added recursion to rmtex.py :D

--- rmtex.py
+++ rmtex.py
@@ -1,24 +1,46 @@
 import os
 import sys
 import re
+import argparse
 
 
-def rmfiles(regexp,dirs):
-	for dir in dirs:
-		files = os.listdir(dir)
-		for file in files:
-			result = re.search(regexp,file)
-			if result:
-				target = os.path.join(os.getcwd(),dir,file)
-				os.remove(target)
-				print('%(file)s removed!' % {'file':target})
-				
-regex = '\w+\.(toc|bbl|aux|gz|blg|dvi|log|out)$'
-print("Removing LaTeX intermediate and log files from:")
-print()
-dirs = [".", "tex"]
-for dir in dirs:
-	print(os.path.join(os.getcwd(),dir))
-print()
-rmfiles(regex,dirs)
-print("Done!")
+def listdir_nohidden(path):
+    no_hidden_list = []
+    for f in os.listdir(path):
+        if not f.startswith('.'):
+            no_hidden_list.append(f)
+    return no_hidden_list
+
+def rmfiles(regexp,cwd,dirs,recursive):
+    for dir in dirs:
+        contents = listdir_nohidden(dir)
+        cwd = os.path.join(cwd,dir)
+        for content in contents:
+            result = re.search(regexp,content)
+            if result:
+                target = os.path.join(cwd,content)
+                os.remove(target)
+                print('%(file)s removed!' % {'file':target})
+            if os.path.isdir(content) and recursive:
+                try:
+                    rmfiles(regexp,cwd,[content],recursive)
+                except:
+                    pass
+
+def main():
+    parser = argparse.ArgumentParser(description = "Cleans up LaTeX log files")
+    parser.add_argument('dirs', nargs='*', default='.')
+    parser.add_argument('-r','--recursive',action='store_true')
+    args=parser.parse_args()
+    regex = '\w+\.(toc|bbl|aux|gz|blg|dvi|log|out)$'
+    print("Removing LaTeX intermediate and log files from:")
+    print("")
+    for dir in args.dirs:
+        print(os.path.join(os.getcwd(),dir))
+    print("")
+    here = os.getcwd()
+    rmfiles(regex,here,args.dirs,args.recursive)
+    print("Done!")
+    
+if __name__ == "__main__":
+    main()
