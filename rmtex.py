b6af13df5e761bf832b8ccb0ae9de261526e5767
Jeff Rabinowitz <rabinowitzjeffc@gmail.com>
2013-05-03 18:34:43 -0400

    Created Skeleton for Report 3.1

--- rmtex.py
+++ rmtex.py
@@ -3,44 +3,52 @@ import sys
 import re
 import argparse
 
-
-def listdir_nohidden(path):
-    no_hidden_list = []
-    for f in os.listdir(path):
-        if not f.startswith('.'):
-            no_hidden_list.append(f)
-    return no_hidden_list
-
-def rmfiles(regexp,cwd,dirs,recursive):
-    for dir in dirs:
-        contents = listdir_nohidden(dir)
-        cwd = os.path.join(cwd,dir)
+def create_regex(reglist):
+    inner = "|".join(reglist)
+    regex = '\w+\.(' + inner + ')$'
+    return regex
+    
+class Remover:
+    """A class to encapsulate removing functionality"""
+    def __init__(self,regexp,dir,recursive):
+        self.regex = regexp
+        self.dir = os.path.abspath(dir)
+        self.recursive = recursive
+    def printmsg(self):
+        print("Removing LaTeX intermediate and log files from:")
+        print(self.dir)
+    def remove(self):
+        self.printmsg()
+        if self.recursive == True:
+            self.remove_recursive()
+        else:
+            self.remove_nonrec(self.dir)
+    def remove_recursive(self):
+        for root, dirs, files in os.walk(self.dir):
+            # dirs[:] = [d for d in dirs if not d[0] == '.']
+            self.remove_nonrec(root)
+    def remove_nonrec(self,path):
+        contents = os.listdir(path)
         for content in contents:
-            result = re.search(regexp,content)
+            result = re.search(self.regex,content)
             if result:
-                target = os.path.join(cwd,content)
+                target = os.path.join(path,content)
                 os.remove(target)
                 print('%(file)s removed!' % {'file':target})
-            if os.path.isdir(content) and recursive:
-                try:
-                    rmfiles(regexp,cwd,[content],recursive)
-                except:
-                    pass
 
-def main():
-    parser = argparse.ArgumentParser(description = "Cleans up LaTeX log files")
-    parser.add_argument('dirs', nargs='*', default='.')
-    parser.add_argument('-r','--recursive',action='store_true')
-    args=parser.parse_args()
-    regex = '\w+\.(toc|bbl|aux|gz|blg|dvi|log|out)$'
-    print("Removing LaTeX intermediate and log files from:")
-    print("")
-    for dir in args.dirs:
-        print(os.path.join(os.getcwd(),dir))
-    print("")
-    here = os.getcwd()
-    rmfiles(regex,here,args.dirs,args.recursive)
+def main(regex,dirs,recursive):
+    for dir in dirs:
+        rm = Remover(regex,dir,recursive)
+        rm.remove()
     print("Done!")
     
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description = "Cleans up LaTeX log files")
+    parser.add_argument('dirs', nargs='*', default='.', help='directories to remove from (default: current)')
+    parser.add_argument('-r','--recursive',action='store_true',help='remove recursively from directories')
+    parser.add_argument('-e','--extensions', nargs='*',default=['toc','bbl','aux','gz','blg','dvi','log','out'],
+        help='specify file extensions to remove (default: LaTeX log files)')
+    args=parser.parse_args()
+    regex = '\w+\.(toc|bbl|aux|gz|blg|dvi|log|out)$'
+    regex = create_regex(args.extensions)
+    main(regex,args.dirs,args.recursive)
